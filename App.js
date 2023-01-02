311c35c032a517536b2355d741865e7547c4d934
Shubham <shubham.singh@gopaisa.in>
2023-01-02 22:45:15 +0530

    react 2nd day

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 const heading=React.createElement("h1",{},"pure react")
 console.log(heading)
 const root= ReactDOM.createRoot(document.getElementById("root"))
